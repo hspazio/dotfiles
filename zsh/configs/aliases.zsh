fc2d2ad208d14dfc1754fd55c6e8e6bb5f87a990
Fabio Pitino <pitinofabio@gmail.com>
2018-04-09 22:59:36 +0100

    add alias for counting references of class/module

--- zsh/configs/aliases.zsh
+++ zsh/configs/aliases.zsh
@@ -10,3 +10,16 @@ alias dkb='dk build'
 alias dku='dk up -d'
 alias dkd='dk down'
 alias dkclean='docker image prune -a && docker volume rm $(docker volume ls -qf dangling=true)'
+
+count_class_references() {
+  find app lib -iname '*.rb' | 
+  xargs grep -h '^[[:space:]]*class\|module\b' | 
+  sed 's/^[[:space:]]*//' | 
+  cut -d ' ' -f 2 | 
+  while read class; do echo `grep -rl "\b$class\b" app lib --include="*.rb" | wc -l` $class; done | 
+  sort -n | 
+  uniq
+}
+alias references=count_class_references
+
+
